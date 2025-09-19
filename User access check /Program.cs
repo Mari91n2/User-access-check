435751a5f0aafb6cee55f24f094e47216ecb0b90
Maryam Ceesay <s235666@dtu.dk>
2025-09-19 16:23:31 +0200

    Aflevering 3

---
+++ User access check /Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Hello, World!");
+
+Console.Write("Username: ");
+string username = Console.ReadLine() ?? "";
+
+Console.Write("Password: ");
+string password = Console.ReadLine() ?? "";
+
+Console.Write("UserId (heltal): ");
+uint userId = uint.TryParse(Console.ReadLine(), out var u) ? u : 0;
+
+
+bool userIsAdmin   = userId > 65536;
+bool usernameValid = username.Length >= 3;
+bool passwordValid =
+    (password.Contains("$") || password.Contains("|") || password.Contains("@")) &&
+    ((userIsAdmin && password.Length >= 20) || (!userIsAdmin && password.Length >= 16));
+
+
+Console.WriteLine();
+Console.WriteLine($"userIsAdmin:   {userIsAdmin}");
+Console.WriteLine($"usernameValid: {usernameValid}");
+Console.WriteLine($"passwordValid: {passwordValid}");
+
+
+
